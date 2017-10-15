eb5fc9599b0ce951886e51df205101b03eee8e47
1iveowl <liveowl@outlook.com>
2017-10-15 20:10:37 +0200

    Refactored namespace

--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -1,10 +1,10 @@
 param([string]$betaver)
 
 if ([string]::IsNullOrEmpty($betaver)) {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.SegmentedControl.Netstandard.dll')).Version.ToString(3)
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.Segmented.dll')).Version.ToString(3)
 	}
 else {
-		$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.SegmentedControl.Netstandard.dll')).Version.ToString(3) + "-" + $betaver
+		$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.Segmented.dll')).Version.ToString(3) + "-" + $betaver
 }
 
 .\build.ps1 $version
