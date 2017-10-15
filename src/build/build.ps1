227d1ac8805b23824fe05ff3e0c9d9c1d10a3cd6
1iveowl <liveowl@outlook.com>
2017-10-15 19:10:21 +0200

    wip

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -8,10 +8,12 @@ param([string]$version)
 if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 
 $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-&$msbuild ..\main\Behaviors.Forms.Xamarin\Behaviors.Forms.Xamarin.csproj /t:Build /p:Configuration="Release"
-&$msbuild ..\main\Behavior.Forms.Xamarin.Netstandard20\Behavior.Forms.Xamarin.Netstandard20.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\main\SegCtlr.Netstandard\SegCtlr.Netstandard.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\crossplatform\SegCtrl.Droid\SegCtrl.Droid.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\crossplatform\SegCtrl.UWP\SegCtrl.UWP.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\crossplatform\SegCtrl.iOS\SegCtrl.iOS.csproj /t:Build /p:Configuration="Release"
 
 
 Remove-Item .\NuGet -Force -Recurse
 New-Item -ItemType Directory -Force -Path .\NuGet
-c:\tools\nuget\NuGet.exe pack Behaviors.Xamarin.Forms.Netstandard.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
+c:\tools\nuget\NuGet.exe pack Plugin.SegmentedControl.Netstandard.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -1,12 +1,12 @@
 param([string]$betaver)
 
 if ([string]::IsNullOrEmpty($betaver)) {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\Behavior.Forms.Xamarin.Netstandard20\bin\Release\netstandard2.0\Behavior.Forms.Xamarin.Netstandard20.dll')).Version.ToString(3)
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.SegmentedControl.Netstandard.dll')).Version.ToString(3)
 	}
 else {
-		$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\Behavior.Forms.Xamarin.Netstandard20\bin\Release\netstandard2.0\Behavior.Forms.Xamarin.Netstandard20.dll')).Version.ToString(3) + "-" + $betaver
+		$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SegCtlr.Netstandard\bin\Release\netstandard1.4\Plugin.SegmentedControl.Netstandard.dll')).Version.ToString(3) + "-" + $betaver
 }
 
 .\build.ps1 $version
 
-c:\tools\nuget\Nuget.exe push ".\NuGet\Behaviors.XamarinForms.$version.symbols.nupkg" -Source https://www.nuget.org
+c:\tools\nuget\Nuget.exe push ".\NuGet\Plugin.SegmentedControl.Netstandard.$version.symbols.nupkg" -Source https://www.nuget.org
